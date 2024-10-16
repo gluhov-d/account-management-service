9b7bf4d942834f24a2531c6b4844f05dcca197f2
gluhov-d <glukhov.d@gmail.com>
2024-10-16 15:33:52 +0300

    fix profilehistorydata

--- src/test/java/com/github/gluhov/accountmanagementservice/service/ProfileHistoryData.java
+++ src/test/java/com/github/gluhov/accountmanagementservice/service/ProfileHistoryData.java
@@ -1,13 +1,10 @@
 package com.github.gluhov.accountmanagementservice.service;
 
 import com.github.gluhov.accountmanagementservice.model.ProfileHistory;
-import com.github.gluhov.dto.ProfileHistoryDto;
 
 import java.util.UUID;
 
-import static com.github.gluhov.accountmanagementservice.service.MerchantsData.MERCHANT_UUID;
 import static com.github.gluhov.accountmanagementservice.service.UsersData.USER_MERCHANT_UUID;
-import static com.github.gluhov.accountmanagementservice.service.UsersData.userMerchantTestDataDto;
 
 public class ProfileHistoryData {
     public static final UUID PROFILE_HISTORY_UUID = UUID.fromString("11111111-2222-3333-4444-555555555555");
